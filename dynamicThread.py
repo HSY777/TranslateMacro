e01a0eee15b9a814ae5ad679888a82ab4533b013
HONG <ongsy1828@naver.com>
2021-04-06 00:54:11 +0900

    update in home 04/06

--- dynamicThread.py
+++ dynamicThread.py
@@ -2,18 +2,85 @@ from tkinter import *
 import tkinter.ttk as ttk
 import threading, queue, time
 
-root = Tk()
-root.title('threading test')
-root.geometry('640x480')
+class Worker(threading.Thread):
+    def __init__(self, name, arrLangCode):
+        super().__init__()
+        self.name = name
+        self.arrLangCode = arrLangCode            # thread 이름 지정
 
-txt = Text(root, height = 1, width = 10)
-txt.pack()
+    def run(self):
+        print("sub thread start ", threading.currentThread().getName())
+        print(self.arrLangCode)
+        time.sleep(3)
+        print("sub thread end ", threading.currentThread().getName())
 
-def getText():
-    result = txt.get('1.0', 'end')
-    print(result)
+def readSentenceFromExcel():
+    startWord = 'startWord11'
+    combine_data = 'combine_data11'
 
-btn = Button(root, text = '읽기', command = getText)
-btn.pack()
+def startTL():
+    readSentenceFromExcel()
+    for i in range(radioval.get()):
+        name = "thread {}".format(i)
+        t1 = Worker(name, arrLangCode[i])
+        t1.start()
 
-root.mainloop()
+def btncmd():
+    startL_count = txt1.get('1.0', 'end')
+    print(startL_count)
+    print(radioval.get())
+
+    t = threading.Thread(target = startTL)
+    t.start()
+    
+def main():
+    txt1.pack()
+    check1.pack()
+    check2.pack()
+    check3.pack()
+    check4.pack()
+    check5.pack()
+    btn.pack()
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('threading test')
+    root.geometry('640x480')
+
+    # TLV
+    startWord = ''
+    combine_data = ''
+
+    # varialbe
+    startL_count = ''
+    arrL_count = 0
+    radioval = IntVar()
+    arrLangCode = ['re', 'en', 'zh', 'ja', 'es']
+
+    txt1 = Text(root, height = 1, width = 10)
+    check1 = Radiobutton(root, text = '1', value = '1', variable = radioval)
+    check2 = Radiobutton(root, text = '2', value = '2', variable = radioval)
+    check3 = Radiobutton(root, text = '3', value = '3', variable = radioval)
+    check4 = Radiobutton(root, text = '4', value = '4', variable = radioval)
+    check5 = Radiobutton(root, text = '5', value = '5', variable = radioval)
+
+    btn = Button(root, text = '시작', command = btncmd)
+
+    main()
+    root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
