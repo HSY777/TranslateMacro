df27c475f9635489ac1504eaaf07e4199aa00c78
HSY777 <ongsy1828@naver.com>
2021-04-11 15:45:37 +0900

    update in lib 04/11

--- dynamicThread.py
+++ dynamicThread.py
@@ -4,12 +4,14 @@ import threading, queue, time
 from selenium import webdriver
 
 class Worker(threading.Thread):
-    def __init__(self, name, arrLangCode):
+    def __init__(self, name, arrLangCode, num):
         super().__init__()
         self.name = name
         self.arrLangCode = arrLangCode            # thread 이름 지정
+        self.num = num
 
     def run(self):
+        
         css_sel = '#yDmH0d > c-wiz > div > div.WFnNle > c-wiz > div.OlSOob > c-wiz > div.ccvoYb > div.AxqVh > div.OPPzxe > c-wiz.P6w8m.BDJ8fb.BLojaf > div.dePhmb > div > div.J0lOec'
         url = 'https://translate.google.co.kr/?hl=' + sel_startL + '&sl=ko&tl=' + self.arrLangCode + '&text=' + combine_startL_sentence + '&op=translate'
 
@@ -18,7 +20,7 @@ class Worker(threading.Thread):
         driver.get(url)  
         driver.implicitly_wait(10)
         rating = driver.find_element_by_css_selector(css_sel)
-        time.sleep(3)
+        #time.sleep(3)
 
         arriveWord = rating.text
         sumWord = ''
@@ -30,8 +32,9 @@ class Worker(threading.Thread):
                 sumWord = ''
 
         #driver.quit()
-        append_result_TL.append(sep_result_TL)
-        print(sep_result_TL)
+        print('완료')
+        append_result_TL.append((sep_result_TL, self.num))
+        print(append_result_TL)
         #print("sub thread start ", threading.currentThread().getName())
         #print(self.arrLangCode)
         #time.sleep(3)
@@ -39,20 +42,32 @@ class Worker(threading.Thread):
 
 def readSentenceFromExcel():
     startWord = 'startWord11'
-    #combine_startL_sentence = '유튜브제목%0A유튜브제목%0A신원길%20멍청한놈%0A유튜브제목%0A유튜브제목%0A신원길%20멍청한놈%0A유튜브제목%0A유튜브제목%0A신원길%20멍청한놈%0A유튜브제목%0A유튜브제목%0A신원길%20멍청한놈%0A헤시태그내용~~~%0A멍청이%0A목아프다%0A'
+    #combine_startL_sentence = '유튜브제목%0A유튜브제목%0A신원길%20안녕하세요%0A유튜브제목%0A유튜브제목%0A신원길%20안녕하세요%0A유튜브제목%0A유튜브제목%0A신원길%20안녕하세요%0A유튜브제목%0A유튜브제목%0A신원길%20안녕하세요%0A헤시태그내용~~~%0A안녕%0A목아프다%0A'
+
+'''def unlockThread(cv):
+    while(condition_count != radioval.get()):
+            print(condition_count)
+            time.sleep(1)
+
+    with cv:
+        cv.notifyAll()
+'''
 
 def startTL():
     readSentenceFromExcel()
     for i in range(radioval.get()):
         name = "thread {}".format(i)
-        t1 = Worker(name, arrLangCode[i])
-        t1.start()
+        num = i
+        TLthread_list.append(Worker(name, arrLangCode[i], num))
+        #t1 = Worker(name, arrLangCode[i], condition)
+        #t1.start()
+    for i in range(radioval.get()):
+        TLthread_list[i].start()
 
 def btncmd():
     #sel_startL = txt1.get('1.0', 'end')
     print(sel_startL)
     print(radioval.get())
-
     t = threading.Thread(target = startTL)
     t.start()
     
@@ -72,14 +87,15 @@ if __name__ == '__main__':
 
     # TLV
     startWord = ''
-    combine_startL_sentence = '유튜브제목%0A유튜브제목%0A신원길%20멍청한놈%0A유튜브제목%0A유튜브제목%0A신원길%20멍청한놈%0A유튜브제목%0A유튜브제목%0A신원길%20멍청한놈%0A유튜브제목%0A유튜브제목%0A신원길%20멍청한놈%0A헤시태그내용~~~%0A멍청이%0A목아프다%0A'
+    combine_startL_sentence = '유튜브제목%0A유튜브제목%0A신원길%20바보%0A유튜브제목%0A유튜브제목%0A신원길%20안녕하세요%0A유튜브제목%0A유튜브제목%0A신원길%20안녕하세요%0A유튜브제목%0A유튜브제목%0A신원길%20안녕하세요%0A헤시태그내용~~~%0A안녕%0A목아프다%0A'
     append_result_TL = []
 
     # varialbe
     sel_startL = 'ko'
     arrL_count = 0
     radioval = IntVar()
-    arrLangCode = ['pt', 'en', 'zh', 'ja', 'es']
+    arrLangCode = ['pt', 'en', 'zh-CN', 'ja', 'es']
+    TLthread_list = []
 
     txt1 = Text(root, height = 1, width = 10)
     check1 = Radiobutton(root, text = '1', value = '1', variable = radioval)
--- threadcondition.py
+++ threadcondition.py
@@ -1,1 +1,31 @@
-https://hamait.tistory.com/753
+import threading, time
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='(%(threadName)-9s) %(message)s',)
+
+def consumer(cv):
+    logging.debug('Consumer thread started...')
+    with cv:
+        logging.debug('Consumer waiting...')
+        cv.wait()
+        time.sleep(3)
+        logging.debug('Consumer consumed the resource')
+
+def producer(cv):
+    logging.debug('Producer thead startd ...')
+    with cv:
+        logging.debug('Making resource available')
+        logging.debug('Notifying to all consumers')
+        cv.notifyAll()
+
+if __name__ == '__main__':
+    condition = threading.Condition()
+    cs1 = threading.Thread(name = 'consumer1', target = consumer, args = (condition,))
+    cs2 = threading.Thread(name = 'consumer2', target = consumer, args = (condition,))
+    pd = threading.Thread(name = 'producer', target = producer, args = (condition,))
+
+    cs1.start()
+    time.sleep(1)
+    cs2.start()
+    time.sleep(2)
+    pd.start()
